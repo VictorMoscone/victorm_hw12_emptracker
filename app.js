5f32bdcfe939daf54644168699fcdad9b011d586
Victor Moscone <victormoscone@gmail.com>
2020-12-31 12:23:15 -0800

    Feat: Inquirer menu that uses switch case to return the result. This was a test.

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 const inquirer = require("inquirer");
+
+const mainMenu = () => {
+    inquirer.prompt({
+        type: "list",
+        message: "Test",
+        name: "menu",
+        choices: ["Test 1", "Test 2"]
+    })
+    .then(({menu}) => {
+        switch (menu) {
+            case "Test 1":
+                console.log("Test 1 says Hi");
+                break;
+            case "Test 2":
+                console.log("Test 2 says Hello");
+                break;
+        };
+    });
+};
+
+mainMenu();
